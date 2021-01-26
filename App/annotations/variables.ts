38eba5038f015208eb0a3337012d756dcf9c17aa
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-27 02:51:57 +0800

    [268] Object Literal Annotations

--- App/annotations/variables.ts
+++ App/annotations/variables.ts
@@ -1,5 +1,5 @@
 let apples: number = 5;
-let speed: string = "fast";
+let speed: string = 'fast';
 let hasName: boolean = true;
 
 let nothingMuch: null = null;
@@ -7,3 +7,18 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4, 5];
+let truths: boolean[] = [true, false, true];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
