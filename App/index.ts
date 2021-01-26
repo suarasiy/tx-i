da6a851e3e4ff707060656936324cbbe58d8c9ed
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-27 02:50:57 +0800

    Tabsize

--- App/index.ts
+++ App/index.ts
@@ -1,27 +1,27 @@
-import axios from "axios";
+import axios from 'axios';
 
-const url = "https://jsonplaceholder.typicode.com/todos/1";
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 interface Todo {
-    id: number;
-    title: string;
-    completed: boolean;
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-axios.get(url).then(response => {
-    const todo = response.data as Todo;
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
 
-    const id = todo.id;
-    const title = todo.title;
-    const completed = todo.completed;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
-    logTodo(id, title, completed);
-})
+  logTodo(id, title, completed);
+});
 
 const logTodo = (id: number, title: string, completed: boolean) => {
-    console.log(`
-        The todo with ID: ${id}
-        Has a title of: ${title}
-        Is it finished? ${completed}
+  console.log(`
+    The todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
     `);
-}
+};
