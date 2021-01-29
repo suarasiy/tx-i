24b8b0768baf4ae032052876f64b74a749f28994
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-29 14:53:33 +0800

    [343] Void and Never

--- App/annotations/functions.ts
+++ App/annotations/functions.ts
@@ -8,3 +8,28 @@ const add = (a: number, b: number): number => {
 const substract = (a: number, b: number) => {
   a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number) {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// Only annotate a function with the type `never`
+// when we really never expect a function return anything ever
+// const throwError = (message: string): never => {
+//   throw new Error(message);
+// }
+
+// but possibly return something, instead using void
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
