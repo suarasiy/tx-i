b202f659b604c272186730e168d1fc93e90c9fbe
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-29 17:56:24 +0800

    [358] Core Reuse with Interfaces

--- App/interfaces/interfaces.ts
+++ App/interfaces/interfaces.ts
@@ -1,7 +1,5 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+// Generic fundamentals and refactoring definition in naming
+interface Reportable {
   summary(): string;
 }
 
@@ -14,8 +12,17 @@ const oldCivic = {
   },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.summary());
+const inDrink = {
+  color: 'green',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
 };
 
-printVehicle(oldCivic);
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
