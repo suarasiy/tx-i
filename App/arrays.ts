e8663c9e33c62faf52443b0d69eabd8886392f37
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-29 15:50:21 +0800

    [347] Why Typed Arrays

--- App/arrays.ts
+++ App/arrays.ts
@@ -3,3 +3,15 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsBySuara: string[][] = [];
+
+// help with inference when extracting values
+const mobil = carMakers[0];
+const myCar = carMakers.pop();
+
+// prevent incompatible values
+// carMakers.push(200);
+
+// help with `map`
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
