00c7b5d71e13e84b07cbfebc4bf5da082ac11baf
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-29 16:25:16 +0800

    [351] Tuples in Action

---
+++ App/tuples.ts
@@ -0,0 +1,11 @@
+const drink = {
+  color: 'green',
+  carbonated: true,
+  sugar: 40,
+};
+
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['green', true, 40];
+const sprite: Drink = ['green', true, 20];
+const tea: Drink = ['crimson', false, 5];
