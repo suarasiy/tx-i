8e83404958ef125ce83246bc1703cfc4142ac599
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-29 14:02:53 +0800

    Inference Around Functions

--- App/annotations/functions.ts
+++ App/annotations/functions.ts
@@ -1,3 +1,10 @@
+// No type inference for arguments!
 const add = (a: number, b: number): number => {
   return a + b;
+};
+
+// Type inference works out output, but we won't use it!
+// If you hover it, popup message will say that function return void
+const substract = (a: number, b: number) => {
+  a - b;
 };
